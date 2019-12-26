bff6c85ec80b56d562850b722c3698b5cd688bbf
William <william.odiomonafe@gmail.com>
2019-12-26 19:05:22 +0100

    changed port to PORT

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const http = require('http');
 const respond = require('./lib/respond.js');
 
 // connection settings
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 // const hostname = '127.0.0.1';
 
 // create server
